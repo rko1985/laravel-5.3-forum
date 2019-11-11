0ea7431248d76d693bcec2c1d0d4e9bafe538817
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-11-11 09:56:15 -1000

    sharing channels with all views

--- resources/views/channels/edit.blade.php
+++ resources/views/channels/edit.blade.php
@@ -1,32 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Edit channel: {{ $channel->title }}</div>
 
-                <div class="panel-body">
-                    <form action="{{ route('channels.update', ['channel' => $channel->id]) }}" value="{{ $channel->title }}" method="POST">  
-                        {{ csrf_field() }}
-                        {{ method_field('PUT') }}
+<div class="panel panel-default">
+    <div class="panel-heading">Edit channel: {{ $channel->title }}</div>
 
-                        <div class="form-group">
-                            <input type="text" name="channel" value="{{ $channel->title }}" class="form-control">
-                        </div>
+    <div class="panel-body">
+        <form action="{{ route('channels.update', ['channel' => $channel->id]) }}" value="{{ $channel->title }}" method="POST">  
+            {{ csrf_field() }}
+            {{ method_field('PUT') }}
 
-                        <div class="form-group">
-                            <div class="text-center">
-                                <button class="btn btn-success" type="submit">
-                                    Update channel
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+            <div class="form-group">
+                <input type="text" name="channel" value="{{ $channel->title }}" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <div class="text-center">
+                    <button class="btn btn-success" type="submit">
+                        Update channel
+                    </button>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
 </div>
+
 @endsection
